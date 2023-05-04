b3d84cdb3f03c1f4c0a5cd9fff41e420e0409e5d
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-05-04 18:14:23 -0400

    refactored RandomQuote component to pass id as prop down to DeleteQuote component

--- src/RandomQuote.jsx
+++ src/RandomQuote.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
+import DeleteQuote from "./DeleteQuote";
 
 function RandomQuote({ quotes }) {
   const [quote, setQuote] = useState("");
+  const [id, setId] = useState("");
 
   function handleClick() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex].quote;
+    const randomId = quotes[randomIndex].id;
     setQuote(randomQuote);
+    setId(randomId);
   }
 
   return (
@@ -14,6 +18,7 @@ function RandomQuote({ quotes }) {
       <h2>Random Quote:</h2>
       <p>{quote}</p>
       <button className="random-button" onClick={handleClick}>Get Random Quote</button>
+      {id && <DeleteQuote id={id} />}
     </div>
   );
 }
