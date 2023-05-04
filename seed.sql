bd199a5583294c7a5e21180eb176728a4efee0b4
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-05-04 13:56:12 -0400

    added data to seed.sql

--- seed.sql
+++ seed.sql
@@ -1,11 +1,11 @@
 INSERT INTO quotes (quote) VALUES 
-('Believe you can and you\'re halfway there.'),
+('Believe you can and you are halfway there.'),
 ('The only way to do great work is to love what you do.'),
-('You miss 100% of the shots you don’t take.'),
-('Don\'t count the days, make the days count.'),
+('You miss 100 percent of the shots you do not take.'),
+('Do not count the days, make the days count.'),
 ('If you want to live a happy life, tie it to a goal, not to people or things.'),
 ('I never make the same mistake twice. I make it like five or six times, you know, just to be sure.'),
-('I don\'t suffer from insanity. I enjoy every minute of it.'),
+('I do not suffer from insanity. I enjoy every minute of it.'),
 ('I always arrive late at the office, but I make up for it by leaving early.'),
-('If at first, you don\'t succeed, skydiving is not for you.'),
+('If at first, you do not succeed, skydiving is not for you.'),
 ('I told my wife she was drawing her eyebrows too high. She looked surprised.');
