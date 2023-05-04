9a0a42531165d458c795b8db63e988f6c78f5c2a
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-05-04 16:26:38 -0400

    affixed all quotes in database to page

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,36 +1,35 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 function Quote() {
   const [quotes, setQuotes] = useState([]);
 
-  function fetchQuote() {
-    fetch('http://localhost:3000/api/quotes')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        setQuotes(data);
-      })
-      .catch(error => {
-        console.log('There was a problem with the fetch operation:', error);
-      });
+  async function fetchQuote() {
+    try {
+      const response = await fetch("http://localhost:3000/api/quotes");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data);
+      setQuotes(data);
+    } catch (error) {
+      console.log("There was a problem with the fetch operation:", error);
+    }
   }
-  
 
   useEffect(() => {
     fetchQuote();
   }, []);
 
+  if (quotes.length === 0) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       {console.log(quotes)}
       {quotes.map((quote) => (
-        <p key={quote.id}>{quote.text}</p>
+        <p key={quote.id}>{quote.quote}</p>
       ))}
     </div>
   );
