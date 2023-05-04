fc232c34c0c42b21b55507c435c631e8a3a1eecb
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-05-04 14:31:34 -0400

    updated server.js to include get route for quotes on api server

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
-import express from 'express';
+import express from "express";
+import process from "process";
 const app = express();
 const PORT = process.env.PORT || 3000;
+import { config } from "dotenv";
+config();
+import pg from "pg";
+const { Pool } = pg;
 
-app.get('/test', (req, res) => {
-  res.json('Hello World!');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.json("Hello World!");
+});
+
+app.get("/api/quotes", (req, res) => {
+  pool.query("SELECT * FROM quotes", (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error querying the database");
+    } else {
+      res.send(result.rows);
+    }
+  });
 });
 
 app.listen(PORT, () => {
